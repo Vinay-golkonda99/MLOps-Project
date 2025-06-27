4b8b62dc6f61f93722ff24999dbacf19b2252ce1
Vinay-golkonda99 <122544244+Vinay-golkonda99@users.noreply.github.com>
2025-06-27 13:57:31 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "vinay-terraform-states"
     key            = "eks/mlops-cluster/terraform.tfstate"
-    region         = "us-east-1"
+    region         = "us-east-2"
     dynamodb_table = "terraform_locks"
     encrypt        = true
   }
