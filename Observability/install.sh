65f693ad8b3469484d3208500ce724e42d1d2392
Vinay-golkonda99 <122544244+Vinay-golkonda99@users.noreply.github.com>
2025-06-30 15:53:23 +0530

    Update install.sh

--- Observability/install.sh
+++ Observability/install.sh
@@ -4,20 +4,19 @@ NAMESPACE="monitoring"
 
 helm repo add grafana https://grafana.github.io/helm-charts
 helm repo update
-
 kubectl create namespace $NAMESPACE --dry-run=client -o yaml | kubectl apply -f -
 
 helm upgrade --install grafana grafana/enterprise-stack \
-  -n $NAMESPACE -f values/grafana-values.yaml
+  -n $NAMESPACE -f observability/grafana/helm-values.yaml
 
 helm upgrade --install loki grafana/loki \
-  -n $NAMESPACE -f values/loki-values.yaml
+  -n $NAMESPACE -f observability/loki/helm-values.yaml
 
 helm upgrade --install promtail grafana/promtail \
-  -n $NAMESPACE -f values/promtail-values.yaml
+  -n $NAMESPACE -f observability/promtail/helm-values.yaml
 
 helm upgrade --install mimir grafana/mimir-distributed \
-  -n $NAMESPACE -f values/mimir-values.yaml
+  -n $NAMESPACE -f observability/mimir/helm-values.yaml
 
 helm upgrade --install tempo grafana/tempo-distributed \
-  -n $NAMESPACE -f values/tempo-values.yaml
+  -n $NAMESPACE -f observability/tempo/helm-values.yaml
